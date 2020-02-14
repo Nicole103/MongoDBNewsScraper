d65284a48a232358bf6c14de7a7a85db5ca1dc31
Nicole Heersink <nheersink8@gmail.com>
2020-02-13 21:29:38 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,47 @@
+//boiler plate
+
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+//port
+var PORT = 3000;
+
+//require models
+var db = require("./models");
+
+//initialize  Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Make public a static folder
+app.use(express.static("public"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+//handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  app.set("view engine", "handlebars");
+  
+  // Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutesb")(app);
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
