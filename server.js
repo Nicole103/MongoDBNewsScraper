e78c243b3c47eb9b5ce59955455b033acfee3137
Nicole Heersink <nheersink8@gmail.com>
2020-02-13 22:51:43 -0700

    adding models and specific page info

--- server.js
+++ server.js
@@ -38,9 +38,58 @@ app.engine(
   );
   app.set("view engine", "handlebars");
   
-  // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutesb")(app);
+// Routes
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+    
+    axios.get("https://www.gameinformer.com/").then(function(response) {
+      // load into cheerio and create $ shorthand
+      var $ = cheerio.load(response.data);
+  
+      // Grabs every h2 within an article tag
+      //find gameinformer's tags we need. 
+      $("article h2").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        //Saves the title and href as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+        result.summary = $(this)
+        .find("p.promo-summary")
+  
+        db.Article.create(result)
+          .then(function(dbArticle) {
+           
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+           
+            console.log(err);
+          });
+      });
+  
+      // Sends user a message 
+      res.send("Scrape Complete");
+    });
+  });
+  
+  // Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // grabs all of the articles
+    db.Article.find({})
+    .then(function(dbArticle){
+        res.json(dbArticle);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+  });
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
